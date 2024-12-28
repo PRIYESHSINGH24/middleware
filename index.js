972a762019d8caf6d01656d6fb3a0736d3c0fd07
PRIYESHSINGH24 <PRIYESHSINGH571@GMAIL.COM>
2024-12-28 22:52:30 +0530

    added the middleware codes in the different form

--- index.js
+++ index.js
@@ -2,25 +2,15 @@ const express = require('express');
 
 const app = express();
 
-function isOldEnough(age){
-    if (age >= 14){
-        return true;
-    } else {
-        return false;
+function standardmiddleware(req, res, next){
+    if (a >= 10){
+        next( );
+    }
+    else {
+        res.json({
+            msg: "Chl aaja kuch aur try karte h"
+        })
     }
 }
 
-app.get('/ride1', function(req, res){
-    if (isOldEnough(req.query.age)) {
-        res.json({
-            msg: "You have sucessfully riden the ride 1"
-        })
-    } else {
-        res.status(411).json({
-            msg: "Sorry Friend"
-        })
-    }
-})
 
-
-app.listen(3000);
---
+++ standardwayofusingmiddlewares.js
@@ -0,0 +1,59 @@
+const express = require('express');
+// an express functionality is a series of middlewares of function is called
+const app = express();
+
+// middleware function are the function which have the req res object and the next object of the file or the data
+
+function isOldEnough(age){
+    if (age >= 14){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function isOldEnoughmiddleware(req, res, next){
+    const age = req.query.age;
+    if (age >= 14){
+        next ( );
+    } else {
+        res.json({
+            msg: "Sorry you are not of age yet",
+        })
+    }
+}
+
+
+app.use(isOldEnoughmiddleware);
+// this should be callled in before functions in the response for the running fo the code
+
+
+app.get('/ride1', function(req, res){
+    if (isOldEnough(req.query.age)) {
+        // req.query.age is for reading the response of the the user fromt he url tab using the 
+        res.json({
+            msg: "You have sucessfully riden the ride 1"
+        })
+    } else {
+        res.status(411).json({
+            msg: "Sorry Friend"
+        })
+    }
+})
+
+app.get('/ride2', function(req, res){
+    // isoldenoughmiddleware is the middleware jo ki sure karega ki function ke run hone se phele woh middleware run ho jaye then woh run hoga 
+    if (isOldEnough(req.query.age)) {
+        // req.query.age is for reading the response of the the user fromt he url tab using the 
+        res.json({
+            msg: "You have sucessfully riden the ride 2"
+        })
+    } else {
+        res.status(411).json({
+            msg: "Sorry Friend"
+        })
+    }
+})
+
+app.listen(3000);
